49fe4cbbed3e3f5a12d7c6f497d2d0a8903e1d1f
Marina FERENT <marinaferent@gmail.com>
2023-07-20 16:39:08 +0300

    Add files via upload
    added the "fixed" parameter

--- R/goodnessFitMatrixPLM.R
+++ R/goodnessFitMatrixPLM.R
@@ -3,6 +3,7 @@
 #' @param x An object of class lm, plm.
 #' @param decim Specifies the number of decimals to display. The default is 4.
 #' @param decim_per Specifies the number of decimals to display in case of percent results. The default is 2.
+#' @param fixed logical. The default is 'FALSE'. If x is a fixed effects model (model="within") use fixed=TRUE.
 #' @returns An n x 1 matrix with the R-squared, Adjusted R-squared, and F-statistic (p-value).
 #' Note that for Fixed-Effects Models, withih R-squareds are provided as given by the plm().
 #' @references
@@ -17,7 +18,7 @@
 #' DVOLS=plm(inv ~ value + capital + factor(firm) + factor(year), data = Grunfeld, model="pooling")
 #' goodnessFitMatrixPLM(DVOLS, 2, 2)
 
-goodnessFitMatrixPLM=function(x, decim=4, decim_per=2)
+goodnessFitMatrixPLM=function(x, decim=4, decim_per=2, fixed=FALSE)
 {
   require(plm)
   goodnessFit=as.data.frame(matrix("",nrow=3,ncol=1))
@@ -26,12 +27,22 @@ goodnessFitMatrixPLM=function(x, decim=4, decim_per=2)
 
   r_squared=unname(summary(x)$r.squared[1])
   goodnessFit[i,1]=paste0(round(r_squared, decim_per+2)*100, "%")
-  rownames(goodnessFit)[i]=paste("R-squared")
+  if(fixed==FALSE)
+  {
+    rownames(goodnessFit)[i]=paste("R-squared")
+  } else {
+    rownames(goodnessFit)[i]=paste("Within R-squared")
+  }
   i=i+1
 
   r_squared_adj=unname(summary(x)$r.squared[2])
   goodnessFit[i,1]=paste0(round(r_squared_adj, decim_per+2)*100, "%")
-  rownames(goodnessFit)[i]=paste("Adj. R-squared")
+  if(fixed==FALSE)
+  {
+    rownames(goodnessFit)[i]=paste("Adj. R-squared")
+  } else {
+    rownames(goodnessFit)[i]=paste("Within Adj. R-squared")
+  }
   i=i+1
 
   F_statistic=unname(summary(x)$fstatistic$statistic)
